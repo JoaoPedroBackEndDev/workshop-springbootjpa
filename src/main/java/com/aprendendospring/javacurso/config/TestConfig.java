21e0d9cf116a2aeede546306562b875fab9539de
JooJ <joaopedroacostaalvarez@gmail.com>
2025-10-01 21:34:30 -0300

    Payment, one-to-one association

--- src/main/java/com/aprendendospring/javacurso/config/TestConfig.java
+++ src/main/java/com/aprendendospring/javacurso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.aprendendospring.javacurso.entities.Category;
 import com.aprendendospring.javacurso.entities.Order;
 import com.aprendendospring.javacurso.entities.OrderItem;
+import com.aprendendospring.javacurso.entities.Payment;
 import com.aprendendospring.javacurso.entities.Product;
 import com.aprendendospring.javacurso.entities.User;
 import com.aprendendospring.javacurso.entities.enums.OrderStatus;
@@ -80,6 +81,13 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 	
 	
